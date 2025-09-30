f3dd9f4f26242c136d811c6d7a2ef396b06700e3
Kozavochka <planchet2013@gmail.com>
2025-09-30 22:09:13 +0300

    add register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { AdminComponent } from './admin/admin.component';
 import {MapComponent} from './map/map.component'
 import { AuthGuard } from './auth.guard';
 import { RequestComponent } from './request/request.component';
+import {RegisterComponent} from './register/register'
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: '',
     component: LayoutComponent,
